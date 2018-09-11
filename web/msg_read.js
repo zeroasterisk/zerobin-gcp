0475d0d9ab872091e5c65f7464133d5439964d69
alan bount <alan@zeroasterisk.com>
2018-09-11 09:07:38 -0600

    add msg_read functionality

--- web/msg_read.js
+++ web/msg_read.js
@@ -1,5 +1,20 @@
+import {
+  newInstance,
+} from './aes_ctr';
+
 // https://jameshfisher.com/2017/11/02/web-cryptography-api-symmetric-encryption.html
+const hex2buf = (hex) => {
+  const bytes = [];
+  for (let i = 0; i < hex.length; i += 2) {
+    bytes.push(Number.parseInt(hex.slice(i, i + 2), 16));
+  }
+  return new Uint8Array(bytes);
+};
+
 export default async function msgRead(key, ciphertext) {
-  const plaintext = 'TODO';
+  const ivSize = 16;
+  const keyArr = hex2buf(key);
+  const cipher = await newInstance(keyArr, ivSize);
+  const plaintext = await cipher.decrypt(hex2buf(ciphertext));
   return plaintext;
 }
